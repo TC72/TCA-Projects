ca91f4f948f8810f5ab05fd616cdffee4553b8d8
Julian Fricker <julianf@digitalbakery.net>
2024-03-25 14:42:36 +0000

    Initial Commit

---
+++ TCA Projects/TCA_ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ProjectsApp.swift
+//  TCA Projects
+//
+//  Created by Julian Fricker on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
